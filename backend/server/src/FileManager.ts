2069f8345b885cd2d86e9b5dd549911b9747d5c8
Anirudh Sriram <hello@techwithanirudh.com>
2025-01-09 07:36:13 +0000

    yes

--- backend/server/src/FileManager.ts
+++ backend/server/src/FileManager.ts
@@ -12,7 +12,7 @@ import { generateFileStructure } from "./utils-filetree"
  * Make sure your Docker image has inotify-tools installed, e.g.:
  *   RUN apt-get update && apt-get install -y inotify-tools
  */
-const PROJECT_DIR = "/home/user/project"
+const PROJECT_DIR = "~/sandbox/backend/server"
 
 export class FileManager {
   private sandboxId: string
